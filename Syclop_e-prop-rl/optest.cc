6ca02ab509785e40c01161f8aa23ef60c49887bb
bnapp <>
2021-04-14 12:16:55 +0300

    Fix optest cv:Mat to tensorflow Tensor conversion and modify toy simulation

--- Syclop_e-prop-rl/optest.cc
+++ Syclop_e-prop-rl/optest.cc
@@ -60,6 +60,7 @@ class TestOp : public OpKernel {
     OP_REQUIRES_OK(context, generator_.Init(context));
 
     cam_heading_ = 0;
+    counter_no_reward_ = 0;
   }
 
   void Compute(OpKernelContext* context) override {
@@ -86,21 +87,23 @@ class TestOp : public OpKernel {
 
     //const int w = ale_.getScreen().width();
     //const int h = ale_.getScreen().height();
-    const int w = 84;
-    const int h = 84;
+    const int w = 160;
+    const int h = 210;
 
     auto local_gen = generator_.ReserveSamples32(1);
     random::SimplePhilox random(&local_gen);
     int to_repeat = frameskip_min_ + random.Uniform(frameskip_max_ - frameskip_min_);
 
-    /*if(should_reset) {
-      ale_.reset_game();
+    if(should_reset) {
+      //ale_.reset_game();
       counter_ = 0;
-      int no_ops = random.Uniform(30);
-      for (int i = 0; i < no_ops; i++) {
-          ale_.act((ale::Action) 0);
-      }
-    }*/
+      //int no_ops = random.Uniform(30);
+      //for (int i = 0; i < no_ops; i++) {
+      //    ale_.act((ale::Action) 0);
+      //    }
+      cam_heading_ = random.Uniform(120)-60;
+      counter_no_reward_ = 0;
+    }
 
     //float r = 0.0;
     for(;to_repeat > 0; --to_repeat){
@@ -109,17 +112,18 @@ class TestOp : public OpKernel {
     }
     counter_ += 1;
 
-    bool done = false;
-    /*bool done = ale_.game_over() || (max_episode_length > 0 && counter_ >= max_episode_length);
-
+    //bool done = ale_.game_over() || (max_episode_length > 0 && counter_ >= max_episode_length);
+    bool done = counter_no_reward_>200 || (max_episode_length > 0 && counter_ >= max_episode_length);
     if(done) {
-      ale_.reset_game();
+      //ale_.reset_game();
       counter_ = 0;
-      int no_ops = random.Uniform(30);
-      for (int i = 0; i < no_ops; i++) {
-          ale_.act((ale::Action) 0);
-      }
-    }*/
+      //int no_ops = random.Uniform(30);
+      //for (int i = 0; i < no_ops; i++) {
+      //    ale_.act((ale::Action) 0);
+      //}
+      cam_heading_ = random.Uniform(120)-60;
+      counter_no_reward_ = 0;
+    }
 
     Tensor* reward_tensor = NULL;
     Tensor* done_tensor = NULL;
@@ -134,18 +138,35 @@ class TestOp : public OpKernel {
 
     auto output_r = reward_tensor->scalar<float>();
     auto output_d = done_tensor->scalar<bool>();
-    auto output_s = screen_tensor->flat<unsigned char>();
+    //auto output_s = screen_tensor->flat<unsigned char>();
+    auto output_tensor_mapped = screen_tensor->tensor<unsigned char, 3>();
 
-    std::vector<unsigned char> screen_buff;
+    //std::vector<unsigned char> screen_buff(h*w*3, 0);
     //ale_.getScreenRGB(screen_buff);
+    
     float r = 0.0;
-    r = getScreenRGB(screen_buff, w ,h);
+    cv::Mat cvScreen = cv::Mat::zeros(h,w,CV_8UC3);
+    r = getScreenRGB(cvScreen, action);
+    if (r<=0.0)
+       counter_no_reward_++;
+    else
+       counter_no_reward_=0;
 
     output_r(0) = r;
     output_d(0) = done;
     //ale_.getScreenRGB(output_s.data());
-    std::copy_n(screen_buff.begin(), h * w * 3,
-		output_s.data()); // get rid of copy?
+    //std::copy_n(screen_buff.begin(), h * w * 3,
+    //		output_s.data()); // get rid of copy?
+
+    for (int y = 0; y < cvScreen.rows; y++) {
+       for (int x = 0; x < cvScreen.cols; x++) {
+          cv::Vec3b pixel = cvScreen.at<cv::Vec3b>(y, x);
+
+          output_tensor_mapped( y, x, 0) = pixel.val[2]; //R
+          output_tensor_mapped( y, x, 1) = pixel.val[1]; //G
+          output_tensor_mapped( y, x, 2) = pixel.val[0]; //B
+       }
+    }
   }
 
 private:
@@ -158,6 +179,7 @@ private:
   int counter_;
   GuardedPhiloxRandom generator_;
   int cam_heading_;
+  int counter_no_reward_;
 
   void act(int action){
      // moving cam heading according to current action
@@ -167,32 +189,33 @@ private:
        cam_heading_++;
 
     // enabling cyclic env
-    if (cam_heading_>84)
-       cam_heading_=-84;
-    else if (cam_heading_<-84)
-       cam_heading_=84;
+    if (cam_heading_>160)
+       cam_heading_=-160;
+    else if (cam_heading_<-160)
+       cam_heading_=160;
   }
 
-  float getScreenRGB(std::vector<unsigned char> &buf, int w, int h){
+  float getScreenRGB(cv::Mat &image, int action){
     float reward = 0.0;
-    cv::Mat image = cv::Mat::zeros( h, w, CV_8UC3 );
+    if (action==1)
+       return reward;
 
     //draw line
     int draw_line_at = 0;
-    if (cam_heading_>=-42 && cam_heading_<42){
-      draw_line_at = 42 - cam_heading_ - 1;
+    if (cam_heading_>=-20 && cam_heading_<20){
+      draw_line_at = image.cols/2 - cam_heading_*(image.cols/40) - 1;
 
       int thickness = 2;
       int lineType = cv::LINE_8;
-      cv::line( image, cv::Point(draw_line_at,0), cv::Point(draw_line_at,h-1),
+      cv::line( image, cv::Point(draw_line_at,0), cv::Point(draw_line_at,image.rows-1),
                 cv::Scalar(250, 250, 250), thickness, lineType );
+
+      reward = 1.0;
     }
-
-    //calc reward
-    if (cam_heading_>=-42 && cam_heading_<42)
-        reward = 5.0;
-
-    buf.assign(image.begin<unsigned char>(), image.end<unsigned char>());
+    else {
+      reward = -1.0;
+    }
+    
     return reward;
   }
 
